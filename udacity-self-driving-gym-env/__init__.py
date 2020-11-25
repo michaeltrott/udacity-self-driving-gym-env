358319dc5318ef63f780df08e813d4b93c5d1ea3
michaeltrott <44398307+michaeltrott@users.noreply.github.com>
2020-11-25 09:28:10 +0000

    Update __init__.py

--- udacity-self-driving-gym-env/__init__.py
+++ udacity-self-driving-gym-env/__init__.py
@@ -2,5 +2,5 @@ from gym.envs.registration import register
 
 register(
     id='udacity-gym-env-v0',
-    entry_point='gym_udacity.envs:UdacityEnv',
+    entry_point='gym_udacity.envs:CustomEnv',
 )
